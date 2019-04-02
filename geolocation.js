e76b54b9830086f1d68e5b9f8096d4030b54df42
mohdafzalwl <49068256+mohdafzalwl@users.noreply.github.com>
2019-04-02 13:34:26 +0530

    get geolocation of a user

---
+++ geolocation.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class App extends React.Component {
+window.navigator.geolocation.getCurrentPosition(
+(positiion) => console.log(position),
+(err)=> console.log(err)
+);
+
+render(){
+return(
+<div>hi</div>
+)
+}}
+
+///examples
